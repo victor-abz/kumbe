360170524fe991e0057d752d8fbca49a537f6d5f
Akimana Jean d Amour <akimanaja17@gmail.com>
2020-09-26 18:04:48 +0200

    Add id key

--- migrations/20200831083220-create-user.js
+++ migrations/20200831083220-create-user.js
@@ -6,7 +6,7 @@ module.exports = {
         allowNull: false,
         primaryKey: true,
         type: Sequelize.UUID,
-        defaultValue: Sequelize.UUIDV4,
+        defaultValue: Sequelize.UUIDV4
       },
       firstName: {
         type: Sequelize.STRING,
--- models/user.js
+++ models/user.js
@@ -3,6 +3,12 @@ export default (sequelize, DataTypes) => {
   const User = sequelize.define(
     'User',
     {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4
+      },
       firstName: DataTypes.STRING,
       lastName: DataTypes.STRING,
       username: {

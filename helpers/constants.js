93438cb989d205f541282c95089d076cf49b7cbb
Akimana Jean d Amour <akimanaja17@gmail.com>
2020-10-06 18:29:15 +0200

    Set up max file size

--- helpers/constants.js
+++ helpers/constants.js
@@ -5,4 +5,4 @@ export const week = day * 7;
 export const getLang = (req) => req.acceptsLanguages('en', 'kin') || 'en';
 export const allowedStrategies = ['google', 'facebook', 'twitter'];
 const MB = 1024 * 1024;
-export const ACCEPTED_FILE_SIZE = 1 * MB; //100 mbs
+export const ACCEPTED_FILE_SIZE = 100 * MB; //100 mbs

4aaebdf688f58e59b0d75095abf27ef1cd8a5034
Akimana Jean d Amour <akimanaja17@gmail.com>
2020-10-23 16:08:08 +0200

    Remove not nullable for brog share

--- migrations/20200906123959-create-blog-share.js
+++ migrations/20200906123959-create-blog-share.js
@@ -1,44 +1,43 @@
 'use strict';
 module.exports = {
-  up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('blog_shares', {
-      id: {
-        allowNull: false,
-        primaryKey: true,
-        type: Sequelize.UUID,
-        defaultValue: Sequelize.UUIDV4,
-      },
-      userId: {
-        type: Sequelize.UUID,
-        allowNull: false,
-        onDelete: 'CASCADE',
-        references: {
-          model: 'users',
-          key: 'id',
-          as: 'userId'
-        }
-      },
-      blogId: {
-        type: Sequelize.UUID,
-        allowNull: false,
-        onDelete: 'CASCADE',
-        references: {
-          model: 'blogs',
-          key: 'id',
-          as: 'blogId'
-        }
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
-  },
-  down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('blog_shares');
-  }
+	up: (queryInterface, Sequelize) => {
+		return queryInterface.createTable('blog_shares', {
+			id: {
+				allowNull: false,
+				primaryKey: true,
+				type: Sequelize.UUID,
+				defaultValue: Sequelize.UUIDV4
+			},
+			userId: {
+				type: Sequelize.UUID,
+				onDelete: 'CASCADE',
+				references: {
+					model: 'users',
+					key: 'id',
+					as: 'userId'
+				}
+			},
+			blogId: {
+				type: Sequelize.UUID,
+				allowNull: false,
+				onDelete: 'CASCADE',
+				references: {
+					model: 'blogs',
+					key: 'id',
+					as: 'blogId'
+				}
+			},
+			createdAt: {
+				allowNull: false,
+				type: Sequelize.DATE
+			},
+			updatedAt: {
+				allowNull: false,
+				type: Sequelize.DATE
+			}
+		});
+	},
+	down: (queryInterface, Sequelize) => {
+		return queryInterface.dropTable('blog_shares');
+	}
 };

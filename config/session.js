3d8131e1711eef73eba4f328b701c3339baf0e6a
Akimana Jean d Amour <akimanaja17@gmail.com>
2020-11-10 16:20:55 +0200

    Handle redis from crushing the app

--- config/session.js
+++ config/session.js
@@ -4,30 +4,33 @@ import redis from 'redis';
 import { hour } from '../helpers/constants';
 
 const RedisStore = connectRedis(expressSession);
-const redisClient = redis.createClient();
+const redisClient = redis.createClient().on('error', (error) => {
+	process.stdout.write(`Redis error: ${error.message}\n`);
+	process.exit(1);
+});
 
 const redisSessionStore = new RedisStore({
-  host: process.env.REDIS_HOST,
-  port: process.env.REDIS_PORT,
-  prefix: process.env.REDIS_PREFIX,
-  name: process.env.REDIS_NAME,
-  pass: process.env.REDIS_SECRET,
-  client: redisClient
+	host: process.env.REDIS_HOST,
+	port: process.env.REDIS_PORT,
+	prefix: process.env.REDIS_PREFIX,
+	name: process.env.REDIS_NAME,
+	pass: process.env.REDIS_SECRET,
+	client: redisClient
 });
 
 export const session = () =>
-  expressSession({
-    resave: true,
-    saveUninitialized: true,
-    secret: process.env.SESSION_SECRET,
-    name: process.env.SESSION_NAME,
-    cookie: {
-      httpOnly: true,
-      secure: false,
-      domain: process.env.BASE_URL,
-      path: '/',
-      sameSite: true,
-      maxAge: 24 * hour
-    },
-    store: redisSessionStore
-  });
+	expressSession({
+		resave: true,
+		saveUninitialized: true,
+		secret: process.env.SESSION_SECRET,
+		name: process.env.SESSION_NAME,
+		cookie: {
+			httpOnly: true,
+			secure: false,
+			domain: process.env.BASE_URL,
+			path: '/',
+			sameSite: true,
+			maxAge: 24 * hour
+		},
+		store: redisSessionStore
+	});

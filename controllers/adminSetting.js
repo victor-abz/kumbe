3438e2f09d653cb28e8bc47a5562a34bfe8c63c0
Akimana Jean d Amour <akimanaja17@gmail.com>
2021-01-11 01:01:32 +0200

    Create controllers got getting and adding

--- controllers/adminSetting.js
+++ controllers/adminSetting.js
@@ -1,8 +1,10 @@
-import { Faq } from '../models';
+import { Faq, SliderContent } from '../models';
 import { getLang, QueryHelper, serverResponse } from '../helpers';
 import { translate } from '../config';
+import { categoryIncludes } from '../helpers/modelIncludes';
 
 const faqDb = new QueryHelper(Faq);
+const sliderDb = new QueryHelper(SliderContent);
 export const addFAQuestion = async (req, res) => {
 	const newFaq = await faqDb.create(req.body);
 
@@ -31,6 +33,29 @@ export const deleteFAQuestion = async (req, res) => {
 	const { questionId: id } = req.params;
 
 	await faqDb.delete({ id });
+
+	const lang = getLang(req);
+	const message = translate[lang].success;
+	return serverResponse(res, 200, message);
+};
+export const createNewSlider = async (req, res) => {
+	const newSlider = await sliderDb.create(req.body);
+
+	const lang = getLang(req);
+	const message = translate[lang].success;
+	return serverResponse(res, 201, message, newSlider);
+};
+export const getSliders = async (req, res) => {
+	const { languageId } = req.body;
+
+	const sliders = await sliderDb.findAll({ languageId }, categoryIncludes);
+	const lang = getLang(req);
+	const message = translate[lang].success;
+	return serverResponse(res, 200, message, sliders);
+};
+export const updateSlider = async (req, res) => {
+	const { sliderId: id } = req.params;
+	await sliderDb.update(req.body, { id });
 
 	const lang = getLang(req);
 	const message = translate[lang].success;
